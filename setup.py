a267c31d17e7f46ea7ea4073da647395b64f7bfe
Nick Ficano <nick@keep.com>
2015-08-16 20:44:26 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import __version__
+import os
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def open_file(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
+
+setup(
+    name="pytube",
+    version=__version__,
+    author="Nick Ficano",
+    author_email="nficano@gmail.com",
+    packages=['pytube'],
+    url="http://pytube.nickficano.com",
+    license=open_file('LICENSE.txt').read(),
+    scripts=['scripts/pytubectl'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Video"
+    ],
+    description="A simple, yet versatile package for downloading "
+                "YouTube videos.",
+    long_description=open_file('README.rst').read(),
+    zip_safe=True,
+)
---
+++ shiver_me_tinders.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import logging
+import yaml
+from collections import namedtuple
+
+import requests
+
+log = logging.getLogger(__name__)
+
+
+class Tinder(object):
+    base_url = "https://api.gotinder.com"
+
+    def __init__(self, fb_token, fb_id, lat, lon):
+        self.fb_token = fb_token
+        self.fb_id = fb_id
+        self.location = {'lat': lat, 'lon': lon}
+        self.auth()
+        self.ping()
+
+    @classmethod
+    def config_from_file(cls, path_to_yaml):
+        with open(path_to_yaml, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        fb_token = cfg.get('facebook', {}).get('token')
+        fb_id = cfg.get('facebook', {}).get('id')
+        lat = cfg.get('location', {}).get('lat')
+        lon = cfg.get('location', {}).get('lon')
+        return cls(fb_token, fb_id, lat, lon)
+
+    def auth(self):
+        endpoint = "/auth"
+        payload = {
+            "locale": "en",
+            "facebook_token": self.fb_token,
+            "facebook_id": self.fb_id
+        }
+        resp = requests.post(self.base_url + endpoint, json=payload)
+        if resp.ok:
+            self.profile = resp.json()
+            self.token = resp.json().get('token', {})
+        if not resp.ok:
+            raise Exception(resp.text)
+        return resp.json()
+
+    def ping(self):
+        endpoint = "/user/ping"
+        headers = {
+            'Token': 'token="%s"' % self.token,
+            'X-Auth-Token': self.token
+        }
+        resp = requests.post(self.base_url + endpoint, headers=headers,
+                             json=self.location)
+        if not resp.ok:
+            raise Exception(resp.text)
+        return resp.json()
+
+    def get_matches(self):
+        User = namedtuple('User', ['id', 'name', 'bio', 'photos'])
+        endpoint = "/user/recs"
+        headers = {
+            'Token': 'token="%s"' % self.token,
+            'X-Auth-Token': self.token
+        }
+        resp = requests.get(self.base_url + endpoint, headers=headers)
+        if not resp.ok:
+            raise Exception(resp.text)
+        for r in resp.json().get('results', []):
+            photos = [p.get('url') for p in r.get('photos')]
+            yield User(id=r.get('_id'), name=r.get('name'), bio=r.get('bio'),
+                       photos=photos)
+
+    def like(self, User):
+        if 'tinder_rate_limited_id_' in User.id:
+            raise Exception(User.bio)
+
+        log.debug('tips hat at %s', User.name)
+        endpoint = "/like/%s" % User.id
+        headers = {
+            'Token': 'token="%s"' % self.token,
+            'X-Auth-Token': self.token
+        }
+        resp = requests.get(self.base_url + endpoint, headers=headers)
+        if not resp.ok:
+            raise Exception(resp.text)
+        return resp.json()
+
+    def dislike(self, User):
+        endpoint = "/pass/%s" % User.id
+        headers = {
+            'Token': 'token="%s"' % self.token,
+            'X-Auth-Token': self.token
+        }
+        resp = requests.get(self.base_url + endpoint, headers=headers)
+        if not resp.ok:
+            raise Exception(resp.text)
+        return resp.json()
+
+
+if __name__ == '__main__':
+    tinder = Tinder.config_from_file('config.yaml')
+    while True:
+        matches = tinder.get_matches()
+        for match in matches:
+            print "liking %s" % match.name
+            tinder.like(match)
